da93c37bfda2161bbf5bcef8fa8ea75e49f1ac59
Pedro Tizl <pedrohtizl@gmail.com>
2020-09-04 21:27:48 -0300

    Initial commit

---
+++ src/main/java/br/uniso/ProjetoAula1Application.java
@@ -0,0 +1,13 @@
+package br.uniso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAula1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAula1Application.class, args);
+	}
+
+}
---
+++ src/main/java/br/uniso/UnisoController.java
@@ -0,0 +1,22 @@
+package br.uniso;
+
+// eh a nossa classe de controller
+// ela pega a requisicao http vinda do browser
+// e processa pra devolver uma resposta
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class UnisoController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam String nome){
+
+        return "Hello " + nome;
+    }
+
+}
